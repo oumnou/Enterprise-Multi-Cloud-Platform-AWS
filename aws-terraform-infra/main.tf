b77ee24e57e85f00beb79d97bc71dbb360db846c
Your Name <you@example.com>
2025-07-14 14:32:24 +0200

    Add IAM users, groups, and group memberships with access policies for project roles

--- aws-terraform-infra/main.tf
+++ aws-terraform-infra/main.tf
@@ -99,3 +99,149 @@ resource "aws_instance" "ec2_private" {
     Name = "private-ec2-1"
   }
 }
+
+# -------------------------------
+# IAM Users
+# -------------------------------
+resource "aws_iam_user" "oumaima" {
+  name = "oumaima.admin"
+}
+
+resource "aws_iam_user" "marouane" {
+  name = "marouane.dev"
+}
+
+resource "aws_iam_user" "sara" {
+  name = "sara.analyst"
+}
+
+resource "aws_iam_user" "laila" {
+  name = "laila.intern"
+}
+
+# -------------------------------
+# IAM Groups
+# -------------------------------
+resource "aws_iam_group" "admins" {
+  name = "admins"
+}
+
+resource "aws_iam_group" "developers" {
+  name = "developers"
+}
+
+resource "aws_iam_group" "analysts" {
+  name = "analysts"
+}
+
+resource "aws_iam_group" "interns" {
+  name = "interns"
+}
+
+# -------------------------------
+# Add Users to Groups
+# -------------------------------
+resource "aws_iam_user_group_membership" "oumaima_membership" {
+  user   = aws_iam_user.oumaima.name
+  groups = [aws_iam_group.admins.name]
+}
+
+resource "aws_iam_user_group_membership" "marouane_membership" {
+  user   = aws_iam_user.marouane.name
+  groups = [aws_iam_group.developers.name]
+}
+
+resource "aws_iam_user_group_membership" "sara_membership" {
+  user   = aws_iam_user.sara.name
+  groups = [aws_iam_group.analysts.name]
+}
+
+resource "aws_iam_user_group_membership" "laila_membership" {
+  user   = aws_iam_user.laila.name
+  groups = [aws_iam_group.interns.name]
+}
+
+# -------------------------------
+# Attach Managed Policies to Groups
+# -------------------------------
+resource "aws_iam_group_policy_attachment" "admin_policy" {
+  group      = aws_iam_group.admins.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "developer_policy" {
+  group      = aws_iam_group.developers.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_group_policy_attachment" "analyst_policy" {
+  group      = aws_iam_group.analysts.name
+  policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
+}
+
+resource "aws_iam_group_policy_attachment" "intern_policy" {
+  group      = aws_iam_group.interns.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+# -------------------------------
+# IAM Access Keys for Users
+# -------------------------------
+resource "aws_iam_access_key" "oumaima_key" {
+  user = aws_iam_user.oumaima.name
+}
+
+resource "aws_iam_access_key" "marouane_key" {
+  user = aws_iam_user.marouane.name
+}
+
+resource "aws_iam_access_key" "sara_key" {
+  user = aws_iam_user.sara.name
+}
+
+resource "aws_iam_access_key" "laila_key" {
+  user = aws_iam_user.laila.name
+}
+
+# -------------------------------
+# Outputs for IAM Access Keys (Sensitive)
+# -------------------------------
+output "oumaima_access_key_id" {
+  value     = aws_iam_access_key.oumaima_key.id
+  sensitive = true
+}
+
+output "oumaima_secret_access_key" {
+  value     = aws_iam_access_key.oumaima_key.secret
+  sensitive = true
+}
+
+output "marouane_access_key_id" {
+  value     = aws_iam_access_key.marouane_key.id
+  sensitive = true
+}
+
+output "marouane_secret_access_key" {
+  value     = aws_iam_access_key.marouane_key.secret
+  sensitive = true
+}
+
+output "sara_access_key_id" {
+  value     = aws_iam_access_key.sara_key.id
+  sensitive = true
+}
+
+output "sara_secret_access_key" {
+  value     = aws_iam_access_key.sara_key.secret
+  sensitive = true
+}
+
+output "laila_access_key_id" {
+  value     = aws_iam_access_key.laila_key.id
+  sensitive = true
+}
+
+output "laila_secret_access_key" {
+  value     = aws_iam_access_key.laila_key.secret
+  sensitive = true
+}
